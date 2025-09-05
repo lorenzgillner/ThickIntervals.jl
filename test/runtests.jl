3bc12c75d524a2b06279bcc60a2fa2c6c7da7d37
gillor <lorenz.gillner@hs-wismar.de>
2025-09-06 00:05:52 +0200

    Apply formatting

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using ThickIntervals
 using Test
 
 A = t2interval(0.5, 1.0, 2.0, 2.5)
-B = t2interval(interval(1.5,2), interval(3.0, 3.5))
+B = t2interval(interval(1.5, 2), interval(3.0, 3.5))
 
 @testset verbose = true "ThickIntervals" begin
 	@testset "addition" begin
