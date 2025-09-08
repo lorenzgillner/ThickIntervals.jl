03b7596571671f76dac7faa1b05433e1d4423806
gillor <lorenz.gillner@hs-wismar.de>
2025-09-08 15:34:47 +0200

    Add proper type conversion and promotion

--- src/ThickIntervals.jl
+++ src/ThickIntervals.jl
@@ -3,32 +3,36 @@
 module ThickIntervals
 
 import IntervalArithmetic: NumTypes, Interval,
-	interval, inf, sup, diam,
-	+, -, *, /,
-	issubset_interval
+    bareinterval, interval, inf, sup, diam,
+    +, -, *, /,
+    issubset_interval
 
 export interval
 
-struct ThickInterval{T <: NumTypes}
-	lo::Interval{T}
-	hi::Interval{T}
+# Construction and basic properties
 
-	function ThickInterval(lo::Interval{T}, hi::Interval{T}) where {T <: Real}
-		if !issubset_interval(interval(sup(lo), inf(hi)), interval(inf(lo), sup(hi)))
-			throw(ArgumentError("Invalid interval bounds"))
-		end
-		new{T}(lo, hi)
-	end
+struct ThickInterval{T<:NumTypes} <: Real
+    lo::Interval{T}
+    hi::Interval{T}
+
+    function ThickInterval(lo::Interval{T}, hi::Interval{T}) where {T<:Real}
+        # TODO bounds checking
+        new{T}(lo, hi)
+    end
 end
 
 export ThickInterval
 
-function t2interval(lo::Interval{T}, hi::Interval{T}) where {T <: Real}
-	ThickInterval(lo, hi)
+function thickinterval(lo::Interval{T}, hi::Interval{T}) where {T<:Real}
+    ThickInterval(lo, hi)
 end
 
-function t2interval(outer_lo::T, inner_lo::T, inner_hi::T, outer_hi::T) where {T <: Real}
-	return t2interval(interval(outer_lo, inner_lo), interval(inner_hi, outer_hi))
+function thickinterval(outer_lo::T, inner_lo::T, inner_hi::T, outer_hi::T) where {T<:Real}
+    return thickinterval(interval(outer_lo, inner_lo), interval(inner_hi, outer_hi))
+end
+
+function thickinterval(x::T) where {T<:Real}
+    return thickinterval(x, x, x, x)
 end
 
 inf(x::ThickInterval) = x.lo
@@ -36,59 +40,101 @@ sup(x::ThickInterval) = x.hi
 outer(x::ThickInterval) = interval(inf(inf(x)), sup(sup(x)))
 inner(x::ThickInterval) = interval(sup(inf(x)), inf(sup(x)))
 
-export t2interval, inf, sup, outer, inner
+export thickinterval, inf, sup, outer, inner
 
-function Base.:+(a::ThickInterval, b::ThickInterval)
-	return t2interval(inf(a) + inf(b), sup(a) + sup(b))
+# Set-like operations
+
+function Base.:>(a::ThickInterval, b::Real)
+    return inf(outer(a)) > b
 end
 
-function Base.:-(a::ThickInterval, b::ThickInterval)
-	return t2interval(inf(a) - sup(b), sup(a) - inf(b))
+function Base.:<(a::ThickInterval, b::Real)
+    return sup(outer(a)) < b
 end
 
-function Base.:*(a::ThickInterval, b::ThickInterval)
-	Φ = [inf(a) * inf(b), inf(a) * sup(b), sup(a) * inf(b), sup(a) * sup(b)]
-	return t2interval(minimum(Φ), maximum(Φ))
-end
-
-function Base.:/(a::ThickInterval, b::ThickInterval)
-	if 0 ∈ outer(b)
-		throw(DivideError("Division by interval containing zero"))
-	end
-
-	inv_b = t2interval(1/sup(b), 1/inf(b))
-
-	return a * inv_b
-end
-
-# TODO relational operators
-
-function inner_diam(a::ThickInterval)
-	return diam(inner(a))
-end
-
-function outer_diam(a::ThickInterval)
-	return diam(outer(a))
-end
-
-function thickness(a::ThickInterval)
-	return outer_diam(a) - inner_diam(a)
+function Base.:∈(x::Real, a::ThickInterval)
+    return x ∈ outer(a)
 end
 
 # TODO use tribool/kleenean here?
 function contains(a::ThickInterval, x::Real)
-	if x ∈ inner(a)
-		return :definitely
-	elseif x ∈ outer(a)
-		return :possibly
-	else
-		return :no
-	end
+    if x ∈ inner(a)
+        return :definitely
+    elseif x ∈ outer(a)
+        return :possibly
+    else
+        return :no
+    end
+end
+
+# Arithmetic operations
+
+function Base.:+(a::ThickInterval, b::ThickInterval)
+    return thickinterval(inf(a) + inf(b), sup(a) + sup(b))
+end
+
+function Base.:-(a::ThickInterval, b::ThickInterval)
+    return thickinterval(inf(a) - sup(b), sup(a) - inf(b))
+end
+
+function Base.:*(a::ThickInterval, b::ThickInterval)
+    Φ = [inf(a) * inf(b), inf(a) * sup(b), sup(a) * inf(b), sup(a) * sup(b)]
+    return thickinterval(minimum(Φ), maximum(Φ))
+end
+
+function Base.:/(a::ThickInterval, b::ThickInterval)
+    if 0 ∈ outer(b)
+        throw(DivideError("Division by interval containing zero"))
+    end
+
+    inv_b = thickinterval(1 / sup(b), 1 / inf(b))
+
+    return a * inv_b
+end
+
+# Utility functions
+
+function inner_diam(a::ThickInterval)
+    return diam(inner(a))
+end
+
+function outer_diam(a::ThickInterval)
+    return diam(outer(a))
+end
+
+function thickness(a::ThickInterval)
+    return outer_diam(a) - inner_diam(a)
 end
 
 function Base.show(io::IO, a::ThickInterval)
-	print(io, "⟦$(inf(a)), $(sup(a))⟧")
+    print(io, "⟦$(inf(a)), $(sup(a))⟧")
 end
 
 export inner_diam, outer_diam, thickness, contains
+
+# Promotion rules
+
+Base.promote_rule(::Type{ThickInterval{T}}, ::Type{ThickInterval{S}}) where {T<:NumTypes,S<:NumTypes} =
+    ThickInterval{promote_type(T, S)}
+
+Base.promote_rule(::Type{ThickInterval{T}}, ::Type{S}) where {T<:NumTypes,S<:Real} =
+    ThickInterval{promote_type(T, S)}
+
+Base.promote_rule(::Type{T}, ::Type{ThickInterval{S}}) where {T<:Real,S<:NumTypes} =
+    ThickInterval{promote_type(T, S)}
+
+# Type conversions
+
+function Base.convert(::Type{ThickInterval{T}}, x::Real) where {T<:NumTypes}
+    x_ = interval(convert(T, x))
+    return thickinterval(x_)
+end
+
+function Base.convert(::Type{ThickInterval{T}}, x::ThickInterval) where {T<:NumTypes}
+    # If already a ThickInterval, convert inner types if needed
+    return ThickInterval(convert(Interval{T}, inf(x)), convert(Interval{T}, sup(x)))
+end
+
+ThickInterval{T}(x::Real) where {T<:NumTypes} = convert(ThickInterval{T}, x)
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,35 @@ using ThickIntervals
 
 using Test
 
-A = t2interval(0.5, 1.0, 2.0, 2.5)
-B = t2interval(interval(1.5, 2), interval(3.0, 3.5))
+@testset verbose = true "ThickIntervals" begin
+    @testset "Creation" begin
+        @testset "From Reals" begin
+            a = thickinterval(0.5, 1.0, 2.0, 2.5)
+            @test inf(inf(a)) == 0.5 && sup(inf(a)) == 1.0 && inf(sup(a)) == 2.0 && sup(sup(a)) == 2.5
+        end
 
-@testset verbose = true "Operators" begin
-	@testset "Addition" begin
-		@test A + B == t2interval(2.0, 3.0, 5.0, 6.0)
-	end
+        @testset "From Intervals" begin
+            a = thickinterval(interval(1.5, 2), interval(3.0, 3.5))
+            @test inf(inf(a)) == 1.5 && sup(inf(a)) == 2.0 && inf(sup(a)) == 3.0 && sup(sup(a)) == 3.5
+        end
+    end
 
-	@testset "Subtraction" begin
-		@test A - B == t2interval(-3.0, -2.0, 0.0, 1.0)
-	end
+    @testset "Operators" begin
+        a = thickinterval(1, 2, 4, 5)
+        b = thickinterval(3, 7, 6, 8)
 
-	@testset "Multiplication" begin
-		@test A * B == t2interval(0.75, 2.0, 6.0, 8.75)
-	end
+        @testset "Addition" begin
+            @test a + b == thickinterval(4, 9, 10, 13)
+        end
+
+        @testset "Subtraction" begin
+            @test a - b == thickinterval(-7, -4, -3, 2)
+        end
+
+        @testset "Multiplication" begin
+            @test a * b == thickinterval(3, 14, 24, 40)
+        end
+
+        # TODO more tests!
+    end
 end
